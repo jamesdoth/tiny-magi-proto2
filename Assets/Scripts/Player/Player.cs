aed78cdc28c74cd626cfa3206774d23018918818
jamesdoth <jamesdoth@gmail.com>
2023-08-28 11:32:00 -0700

    NEW BASELINE, camera, animations, player script

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 5f;
+    public int maxHealth = 100;
+    private int currentHealth;
+    private bool isAlive;
+
+    private Rigidbody2D rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        isAlive = true;
+        currentHealth = maxHealth;
+    }
+    private void Update()
+    {
+        if (isAlive)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector2 movement = new Vector2(horizontalInput, verticalInput);
+            rb.velocity = movement * speed;
+        }
+
+        if (currentHealth > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TakeDamage(10);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        currentHealth -= damage;
+        Debug.Log("Player health: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isAlive = false;
+        Destroy(gameObject);
+        Debug.Log("Player has died.");
     }
 }
